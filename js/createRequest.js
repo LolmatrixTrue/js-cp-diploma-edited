da82acabd47a813e5aa9ccbaaef04d3028703c16
LolmatrixTrue <66357036+LolmatrixTrue@users.noreply.github.com>
2023-06-20 21:27:37 +0300

    ready

--- js/createRequest.js
+++ js/createRequest.js
@@ -1,11 +1,26 @@
-function getRequest(body, callback = (response) => {}) {
-    let xhr = new XMLHttpRequest();
-
-  xhr.open("POST", "https://jscp-diplom.netoserver.ru/", true);
-  xhr.responseType = "json";
+const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(options.method || "POST", options.url, true);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(body);
-  xhr.onload = () => {
-    callback(xhr.response);
-  };
-}
+  xhr.send(options.params);
+  xhr.onreadystatechange = () => {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				try {
+					const response = JSON.parse(xhr.responseText);
+					for (let key in response) {
+						if (response[key].err) {
+							alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
+							return;
+						}
+					}
+					options.callback(response);
+				} catch (err) {
+					alert(`Ошибка ответа: ${err.message}`);
+				}
+			} else {
+				alert(`Ошибка запроса: ${xhr.status} ${xhr.statusText}`);
+			}
+		}
+	};
+};
